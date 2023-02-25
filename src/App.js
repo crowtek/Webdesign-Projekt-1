6fd51480905bafb258691534457b8b4f2c57067e
crowtek <prt3@hotmail.de>
2023-02-25 22:31:46 +0100

    Added section-5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Section1 } from './sections/section-1';
 import { Section2 } from './sections/section-2';
 import { Section3 } from './sections/section-3';
 import { Section4 } from './sections/section-4';
+import { Section5 } from './sections/section-5';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
     </div>
   );
 }
---
+++ src/sections/section-5.js
@@ -0,0 +1,23 @@
+
+import "./section-5.css";
+
+export function Section5() {
+    return (
+        <section className="section-5">
+            <div className="section-5-text-container">
+                <h1>BUSINESS PLAN</h1>
+                <p>
+                    Our business plan outlines our vision, goals, and strategies 
+                    for success. It's the result of careful research and analysis, 
+                    and includes a marketing and sales strategy, financial projections, 
+                    and more. We use it as a roadmap for our growth, and to communicate 
+                    our vision and goals to stakeholders. Contact us to learn more.
+                </p>
+            </div>
+
+            <div className="section-5-img">
+                <div className="diagramImg"></div>
+            </div>
+        </section>
+    );
+}
